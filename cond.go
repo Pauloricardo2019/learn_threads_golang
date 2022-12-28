965d48a379fc3ee81c0f8f550ceb5cd951a3aff6
Pauloricardo2019 <paullin_santos13@hotmail.com>
2022-12-28 11:58:40 -0300

    lean_threads

---
+++ cond.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const (
+	tam = 10
+)
+
+var (
+	values [tam]string
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+	
+	cond := sync.NewCond(&sync.Mutex{})
+
+	for i := 0; i < tam; i++ {
+		d := time.Second * time.Duration(rand.Intn(10)) / 10
+		go func(i int) {
+			time.Sleep(d)
+			cond.L.Lock()
+			values[i] = string('a' + i)
+			cond.L.Unlock()
+			cond.Signal()
+		}(i)
+	}
+
+	cond.L.Lock()
+	defer cond.L.Unlock()
+	for !checkCondition() {
+		cond.Wait()
+	}
+
+}
+
+func checkCondition() bool {
+	fmt.Println(values)
+	for i := 0; i < tam; i++ {
+		if values[i] == "" {
+			return false
+		}
+	}
+	return true
+}
