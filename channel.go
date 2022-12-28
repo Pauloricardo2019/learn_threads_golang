14caa8fb2001cc6d3157842377d4382c33377534
Pauloricardo2019 <paullin_santos13@hotmail.com>
2022-12-28 11:57:16 -0300

    lean_threads

---
+++ channel.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+
+	rand.Seed(time.Now().UnixNano())
+
+	ch := make(chan int)
+
+	go func() {
+
+		sum := rand.Int() + rand.Int()
+
+		ch <- sum
+	}()
+
+	result := <-ch
+
+	fmt.Println("Result: ", result)
+
+}
---
+++ goroutines.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+
+	alf := [10]string{
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+	}
+
+	var wp sync.WaitGroup
+	wp.Add(2)
+	go func() {
+		defer wp.Done()
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 300)
+			fmt.Println("goroutine 1: ", i)
+		}
+	}()
+
+	go func() {
+		defer wp.Done()
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Millisecond * 300)
+			fmt.Println("goroutine 2: ", alf[i])
+		}
+	}()
+
+	wp.Wait()
+}
