5aaf654a581daeb049f14137aacc3af73f05d887
Pauloricardo2019 <paullin_santos13@hotmail.com>
2022-12-30 15:07:11 -0300

    implements semaforo

--- semaforo.go
+++ semaforo.go
@@ -15,6 +15,7 @@ type register struct {
 
 var (
 	wp sync.WaitGroup
+	mx sync.Mutex
 )
 
 func main() {
@@ -22,13 +23,16 @@ func main() {
 	semaforo := make(chan struct{}, 10)
 
 	for {
-		wp.Add(1)
+		wp.Add(2)
 
+		semaforo <- struct{}{}
 		semaforo <- struct{}{}
 
 		go func(semaforo <-chan struct{}) {
 			defer wp.Done()
-			fmt.Println(fmt.Sprintf("semaforo position: %d", len(semaforo)))
+			fmt.Println(fmt.Sprintf("semaforo count: %d", len(semaforo)))
+			mx.Lock()
+			defer mx.Unlock()
 			err := conditionalError()
 			if err != nil {
 				fmt.Println("deu erro aqui.")
@@ -37,12 +41,26 @@ func main() {
 			}
 			<-semaforo
 		}(semaforo)
+
+		go func(semaforo <-chan struct{}) {
+			defer wp.Done()
+			fmt.Println(fmt.Sprintf("semaforo count: %d", len(semaforo)))
+			err := conditionalError()
+			if err != nil {
+				fmt.Println("deu erro aqui.")
+				<-semaforo
+				return
+			}
+			<-semaforo
+		}(semaforo)
+
 		wp.Wait()
-		time.Sleep(time.Millisecond * 300)
+
 	}
 }
 
 func conditionalError() error {
+	time.Sleep(500 * time.Millisecond)
 	numberRandom := rand.Int()
 	if numberRandom%2 == 0 {
 		return errors.New("this number is pair")
